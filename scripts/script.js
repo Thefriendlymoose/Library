453f632a25d5dc868558fccb2bb0087435f5461b
Thefriendlymoose <alamiridavid@gmail.com>
2021-03-19 00:00:55 +0100

    added styling to header and add button, created modal functionality, book functionality finished, next fine tune styling and add local storage functionality

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,23 @@ const read = document.getElementById("read");
 const addBook = document.getElementById("add-book");
 const form = document.getElementById("form");
 
+const cardDeleteButtons = document.getElementsByClassName("delete-card");
 
+
+
+function deleteCard(card){
+    myLibrary.splice(card.target.parentNode.dataset.index, 1);
+    booksToDom();
+}
+
+function changeReadStatus(card){
+    console.log(card.target.parentNode.parentNode.dataset.index);
+    myLibrary[card.target.parentNode.parentNode.dataset.index].readBook();
+}
 
 let myLibrary = [];
+
+// Book Constructor
 
 function Book(title, author, pages, desc, read) {
     this.title = title;
@@ -20,49 +34,113 @@ function Book(title, author, pages, desc, read) {
     this.pages = pages;
     this.desc = desc;
     this.read = read;
-    readBook = function(){
+    this.readBook = function(){
+        if(this.read){
+            this.read = false;
+        } else{
+            this.read = true
+        }
+    };
+}
 
-    }
+function booksToDom(){
+    while (lib.firstChild) {
+        lib.removeChild(lib.lastChild);
+      }
+    myLibrary.forEach((book, index) => {
+        createBookCard(book, index);
+    })
 }
 
 function addBookToLibrary(book){
     myLibrary.push(book);
-    createBookCard(book);
+    booksToDom();
 }
 
-function createBookCard(book){
+function createBookCard(book, serialNumber){
     const newContainer = document.createElement("div");
     newContainer.classList.add("book-card");
+    newContainer.setAttribute("data-index", serialNumber);
     const title = document.createElement("h2");
     const author = document.createElement("h3");
     const pages = document.createElement("h4");
     const desc = document.createElement("p");
-    const read = document.createElement("p");
+    
+    const readContainer = document.createElement("div");
+    const read = document.createElement("input");
+    const readLabel = document.createElement("label");
+    read.type = "checkbox";
+    read.name = "bookread" + serialNumber;
+    read.id = "bookread" + serialNumber;
+    readLabel.htmlFor = "bookread" + serialNumber;
+    readLabel.textContent = "Book Read: ";
 
+    if(book.read){
+        read.checked = true;
+    } else{
+        read.checked = false;
+    }
 
+    read.addEventListener("click", changeReadStatus);
+
+    readContainer.appendChild(readLabel);
+    readContainer.appendChild(read);
+    
+    const close = document.createElement("span");
+    close.classList.add("delete-card");
+
+    close.addEventListener("click", deleteCard);
+
+    close.innerHTML = "&times";
     title.innerText = book.title;
     author.innerText = book.author;
     pages.innerText = `Pages: ${book.pages}`;
     desc.innerText = book.desc;
-    read.innerText = book.read;
 
+    newContainer.appendChild(close);
     newContainer.appendChild(title);
     newContainer.appendChild(author);
     newContainer.appendChild(pages);
     newContainer.appendChild(desc);
-    newContainer.appendChild(read);
+    newContainer.appendChild(readContainer);
 
     lib.appendChild(newContainer);
 }
+
+// Add new book
+
+form.addEventListener("submit", logSubmit);
 
 function logSubmit(event) {
     let newBook = new Book(bookName.value, author.value, pages.value, desc.value, read.checked);
     addBookToLibrary(newBook);
     event.preventDefault();
     event.target.reset();
-
-  }
-
-form.addEventListener("submit", logSubmit);
+    modal.style.display = "none";
+}
 
 
+// Book form popup
+
+const modal = document.getElementById("modal")
+const showForm = document.getElementById("show-form")
+const closeForm = document.getElementById("close-btn")
+
+showForm.addEventListener("click", openModal)
+closeForm.addEventListener("click", closeModal)
+window.addEventListener("click", outsideClick);
+
+function openModal(){
+    modal.style.display = "block";
+}
+
+function closeModal(){
+    modal.style.display = "none";
+}
+
+function outsideClick(e){
+    if(e.target == modal){
+        modal.style.display = "none";
+    }
+    
+}
